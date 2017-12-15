6c0b51a1f7d9331326d8df38d181c1d8d8233ce7
Frederico b.k <frederico.klein@plymouth.ac.uk>
2017-12-15 17:04:40 +0000

    autogenerated urdf and srdf files based on xacro templates

--- scripts/make_urdf.sh
+++ scripts/make_urdf.sh
@@ -4,6 +4,7 @@
 # the paths are going to look bad, but that is how I got this working. If you
 # want to fiddle with cmake variables and pass the right working directories,
 # please do and update all these paths.
+# UPDATE: Added part to generate SRDF file as well. Probably should rename separate these files
 
 echo "** Creating URDF file from gummi_description_base.urdf.xacro and gummi_description_ee.urdf.xacro."
 
@@ -12,3 +13,7 @@ echo "** Creating URDF file from gummi_description_base.urdf.xacro and gummi_des
 export GUMMIBASEPATH=${PWD}
 export GUMMIEEPATH=${PWD}/../gummi_ee_${ROS_GUMMI_EE}
 rosrun xacro xacro --inorder ${GUMMIBASEPATH}/xacro/gummi_description.urdf.xacro base:=${GUMMIBASEPATH} ee:=${GUMMIEEPATH} > ${GUMMIBASEPATH}/urdf/gummi_description.urdf
+
+echo "** Creating SRDF file from gummi_base.srdf.xacro and gummi_ee.srdf.xacro."
+
+rosrun xacro xacro --inorder ${GUMMIBASEPATH}/xacro/gummi.srdf.xacro base:=${GUMMIBASEPATH} ee:=${GUMMIEEPATH} > ${GUMMIBASEPATH}/config/gummi.srdf
